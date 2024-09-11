47f376fb13663ce2591d7dffb97b9b2f043375c8
leelike123 <leelike123@email.daelim.ac.kr>
2024-09-11 17:19:12 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 100;
+        boolean c = true;
+        char d = 63;
+        String e = "";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+
+}
---
+++ src/QuestionWeek2.java
@@ -0,0 +1,9 @@
+public class QuestionWeek2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+        b = 20;
+        System.out.println(a);
+
+    }
+}
